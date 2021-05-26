54f4c1fbee46e78a8079bbf0e1aa0b9b9db0f9db
Jen S <j3n.s3idl@gmail.com>
2021-05-26 16:17:03 -0400

    converting dictionary to dataframe

--- create_budget_dict.py
+++ create_budget_dict.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-## Create budget dict. py takes a given csv and sorts the data from the csv
+# Create budget dict. py takes a given csv and sorts the data from the csv
 # into a dictionary with a budget category as the key
 # it also asks the user what are the necessary columns for their budget
 # once the program begins it passes a unique identifier to each row entry
@@ -9,9 +9,17 @@
 # so that once it is within the dictionary the program will recognize
 # all future occurences of identifier and add it without input
 
+from fastnumbers import fast_float
+import plotly.graph_objects as go
+import squarify
+from matplotlib.lines import Line2D
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+import matplotlib
+import pprint
 import platform
 
-# working with dates, month abbrev, and the 
+# working with dates, month abbrev, and the
 #  it takes to run prg
 from datetime import datetime
 from datetime import date
@@ -24,31 +32,27 @@ import time
 import pandas as pd
 import numpy as np
 pd.options.mode.chained_assignment = None
-import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 # graphing
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-from matplotlib.lines import Line2D
-import squarify
-import plotly.graph_objects as go
 
 # Dealing with number input inconsistencies, avoids VALUE ERROR, ATTRIBUTE ERROR
-from fastnumbers import fast_float
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 # most of the data I began to work with to generate the working test data
 # i had to get from the clipboard on websites
+
+
 def reading_clipboard():
     clipDF = pd.read_clipboard()
     csvName = input('What type of account is it?')
     csvName = csvName + '.csv'
-    clipDF.to_csv(csvName, index = False)
+    clipDF.to_csv(csvName, index=False)
 
-## Need to add in portion from other notebook that asked user if they want to install these dependencies if they are not included
+# Need to add in portion from other notebook that asked user if they want to install these dependencies if they are not included
+
+
 def version_assistant():
     # print versions
     print('//////////////////////////////////////////////////////////////////////////////////////////////////////')
@@ -60,6 +64,7 @@ def version_assistant():
     print("squarify   :  0.4.3")
     print('-----------------------------------------\n\n')
 
+
 def read_csv():
     csvName = input('ENTER CSV NAME\n')
     print('-----------------------------------------')
@@ -69,26 +74,31 @@ def read_csv():
         csvName = csvName + '.csv'
         return pd.read_csv(csvName)
 
+
 def get_sort_by(df):
     pp.pprint(df.head())
     print('------------------------------------------------------------------------------------------------------')
     print('OPTIONS:::')
     print('------------------------------------------------------------------------------------------------------')
     #none_in = [print(list(df.columns)[i].upper(), end =" ") for i in range(len(list(df.columns)))]
-    sort_col = input(f'\n\nCHOOSE COLUMN TO SORT BUDGET CATEGORIES BY?:: \n').lower()
-    sort = ' '.join(str(elem) for elem in [i for i in df.columns if i == sort_col.lower()])
+    sort_col = input(
+        f'\n\nCHOOSE COLUMN TO SORT BUDGET CATEGORIES BY?:: \n').lower()
+    sort = ' '.join(str(elem)
+                    for elem in [i for i in df.columns if i == sort_col.lower()])
     return sort
+
 
 def get_categories():
     print('//////////////////////////////////////////////////////////////////////////////////////////////////////')
-    defaults = ['food', 'fast_food', 'home', 'pets', 'restaurants', 'utilities', 'car', 'gas', 'medical', 'fun', 'family', 'deposits', 'interest']
+    defaults = ['food', 'fast_food', 'home', 'pets', 'restaurants', 'utilities',
+                'car', 'gas', 'medical', 'fun', 'family', 'deposits', 'interest']
     print(f'\nDEFAULT CATEGORIES::: \n{" - ".join(defaults)}\n')
     use_defaults = input('USE DEFAULTS::: Y/N\n')
     if 'y' in use_defaults:
         categories = defaults
     else:
         categories = input(
-        'ENTER BUDGET CATEGORIES? USE UNDERSCORES:::\n')
+            'ENTER BUDGET CATEGORIES? USE UNDERSCORES:::\n')
     cat = ''
     if type(categories) == type('s'):
         for i in categories:
@@ -99,7 +109,9 @@ def get_categories():
         categories = cat.split(' ')
     return categories
 
-## ADD EXIT QUERY FOR LOOP
+# ADD EXIT QUERY FOR LOOP
+
+
 def add_trans_type(df, i, sort_by=0):
     ####################################################get_sort_by####################################################
     if sort_by:
@@ -109,17 +121,20 @@ def add_trans_type(df, i, sort_by=0):
             key = input(
                 f'SORT THIS PURCHASE BY:: {purchase_type[index]}, Y/N\n')
             if 'y' in key:
-                print('//////////////////////////////////////////////////////////////////////////////////////////////////////')
+                print(
+                    '//////////////////////////////////////////////////////////////////////////////////////////////////////')
                 print(
                     f'YOU WILL SORT THIS TRANS BY COLUMN:: {str(sort_by)} DATA::{purchase_type[index]}\n')
                 return sort_by, purchase_type[index]
             else:
-                print('//////////////////////////////////////////////////////////////////////////////////////////////////////')
+                print(
+                    '//////////////////////////////////////////////////////////////////////////////////////////////////////')
                 print('CHOOSE AGAIN')
                 continue
 
+
 def make_dict(categories):
-    trans_type = {'0_format':[
+    trans_type = {'0_format': [
         'date', 'transaction', 'amount', 'identifier']}
     for i in categories:
         trans_type[i] = []
@@ -131,10 +146,11 @@ def add_data(budget_dict, data):
     print('//////////////////////////////////////////////////////////////////////////////////////////////////////')
     location = str(input(
         f'\nCHOOSE CATEGORY FOR::: "{data[1]}"\n------------------------------------------------------\nCATEGORY OPTIONS:: \n\n{" - ".join(list(budget_dict.keys()))}\n------------------------------------------------------\n'))
-    
+
     # Adding a new key if the entered key is not already in the dictionary or part of defaults
     if location not in budget_dict.keys():
-        add_key = input(f'{location}:: NOT IN BUDGET FILE, WOULD YOU LIKE TO ADD IT? Y/N\n')
+        add_key = input(
+            f'{location}:: NOT IN BUDGET FILE, WOULD YOU LIKE TO ADD IT? Y/N\n')
         if 'y' in add_key:
             budget_dict[location] = []
 
@@ -199,7 +215,7 @@ def split_purchases(df, budget_dict=0):
         trans_type = make_dict(categories)
         ############################################## get_sort_by ##############################################
         sort_by = get_sort_by(df)
-        
+
 
 # this block asks the reader what columns to keep within the list of data
 # Need to add functionality that allows for spaces, and the word and and splits on spaces
@@ -220,8 +236,7 @@ def split_purchases(df, budget_dict=0):
         except KeyError:
             print('ERROR, RE-ENTER COLUMN\n')
 
-
-    for i in range(len(df)): 
+    for i in range(len(df)):
         ##############################################add_trans_type ###############################################
         get_col = add_trans_type(df, i, sort_by)
         organize_by = get_col[0]
@@ -254,47 +269,79 @@ def read_clip():
 
 # First iteration assumed that data was perfect, all categories full, equal amounts of purchases ea category
 # Need to code for zero data in categories and fluctuations in purchases for ea cat type
-def dict_to_Frame(data):
-    df = pd.DataFrame()
-    imported = pd.DataFrame.from_dict(data, orient='index')
-    imported.reset_index(inplace = True)
-# Adding the category key to the end of each list of data to later add to df as column
-# Try accept statement and dropna clears out categories that are not used/empty
-    for i in range(len(imported[0])):
-        try:
-            imported[0][i].append(imported['index'][i])
-        except AttributeError:
+# def dict_to_Frame(data):
+#     df = pd.DataFrame()
+#     imported = pd.DataFrame.from_dict(data, orient='index')
+#     imported.reset_index(inplace = True)
+# # Adding the category key to the end of each list of data to later add to df as column
+# # Try accept statement and dropna clears out categories that are not used/empty
+#     for i in range(len(imported[0])):
+#         try:
+#             imported[0][i].append(imported['index'][i])
+#         except AttributeError:
+#             continue
+#     for i in range(len(imported[0])):
+#         df = pd.concat([df, pd.DataFrame([imported[0][i]])])
+#     col_names = df.iloc[0]
+#     for i in range(len(col_names)):
+#         df = df.rename(columns={df.columns[i]: col_names[i]}).reset_index(drop=True)
+#     df = df.drop([0,0]).reset_index(drop=True)
+#     df = df.rename(columns = {'format':'category'})
+#     df = df.dropna().reset_index(drop=True)
+#     return df
+
+
+def dict_to_Frame(data_dict):
+    skip_list = []
+    for key, value in data_dict.items():
+    # Skipping the first entry which is the columns
+        if key == '0_format':
             continue
-    for i in range(len(imported[0])):
-        df = pd.concat([df, pd.DataFrame([imported[0][i]])])
-    col_names = df.iloc[0]
-    for i in range(len(col_names)):
-        df = df.rename(columns={df.columns[i]: col_names[i]}).reset_index(drop=True)
-    df = df.drop([0,0]).reset_index(drop=True)
-    df = df.rename(columns = {'format':'category'})
-    df = df.dropna().reset_index(drop=True)
+        elif len(value) == 0:
+            skip_list.append(key)
+        else:
+            # print(f'{value}')
+            db = pd.DataFrame()
+            li = []
+            for i in range(len(value)):
+                li.append(value[i])
+    print((f'{skip_list} = NONE, SKIPPING'))
+    df = pd.DataFrame(np.array(li), columns=data_dict['0_format'])
     return df
 
 # Adding PYMONGO DB functionality
+# Need to add a print statement, also pymongo install based on the users OS
+# For consistent use of program need to add searchability that the db connection only adds if the data is new
+
+
 def conn_mongo(data):
     import pymongo
     conn = 'mongodb://localhost:27017'
     client = pymongo.MongoClient(conn)
     db = client.clover
+    if db.budgetDB.drop():
+        print('MAKING NEW DATABASE')
+    else:
+        print('DB NOT PRESENT')
+    db.show
     db.budgetDB.insert_one(data)
-    #db.budgetDB.find().pretty()
+
+    # Hopefully printing the first 5 entries in the db
+    # pprint(db.budgetDB.find_one())
+    # db.budgetDB.find().pretty()
+
 
 def main():
-# WORKING< 
-# change csv variable to data
-# add loop that continues until a yes or no is given or an exit request
+    # WORKING<
+    # change csv variable to data
+    # add loop that continues until a yes or no is given or an exit request
 
     csv_or_clip = input('DATA IS CSV OR CLIPBOARD INPUT?\n').lower()
     if 'csv' in csv_or_clip:
         # Normalizing the column names to lower
         data = read_csv()
         for i in data.columns:
-            data = data.rename(columns={i:i.lower()})
+            data = data.rename(columns={i: i.lower()})
         print('IMPORTED DATASET\n')
         print('------------------------------------------------------------------------------------------------------')
         pp.pprint(data.head())
@@ -310,7 +357,7 @@ def main():
             data = read_clip()
             data = format_data.initiate_format(data)
             for i in data.columns:
-                data = data.rename(columns={i:i.lower()})
+                data = data.rename(columns={i: i.lower()})
             print('IMPORTED DATASET\n')
             print('------------------------------------------------------------------------------------------------------')
             pp.pprint(data.head())
@@ -325,24 +372,17 @@ def main():
     trans_dict = split_purchases(data.head(1))
     pp.pprint(trans_dict)
     print('------------------------------------------------------------------------------------------------------')
-    
-    #Need to add dictionary to DB functionality, right now won't work because there are empty categories, and unequal values in columns
-    #converted_DF = dict_to_Frame(data)
-    #pp.pprint(converted_DF)
-    conn_mongo(trans_dict)
-    print('MongoDB Successful')
-    return data#, converted_DF
 
+    # Need to add dictionary to DB functionality, right now won't work because there are empty categories, and unequal values in columns
+    converted_DF = dict_to_Frame(data)
+    pp.pprint(converted_DF)
+    create_database = input('ADD TO DATABASE? Y/N')
+    if 'y' in create_database:
+        conn_mongo(trans_dict)
+        print('MongoDB Successful')
+    return data  # , converted_DF
 
     #pp.pprint(pd.DataFrame.from_dict(data, orient='index', columns = data.keys()))
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -353,15 +393,15 @@ if __name__ == "__main__":
 dictionary = {
     'format': ['date', 'location data', 'float amount', 'identifier'],
     'home': [
-    ['01/24/21', 'HOME_DEPOT',  -57, 'HOME'],
-    ['01/12/21', 'LOWES', -100, 'LOWES'],
-    ['02/14/21', 'TRUE_VALUE', -60, 'TRUE']],
+        ['01/24/21', 'HOME_DEPOT',  -57, 'HOME'],
+        ['01/12/21', 'LOWES', -100, 'LOWES'],
+        ['02/14/21', 'TRUE_VALUE', -60, 'TRUE']],
 
     'fast_food': [
-    ['01/28/21', 'CHICK-FIL-A', -14.99, 'CHICK-FIL-A']],
+        ['01/28/21', 'CHICK-FIL-A', -14.99, 'CHICK-FIL-A']],
 
-    'food':[
-    ['01/22/21', 'FOOD LION',  -200, 'FOOD LION'],
-    ['02/21/21', 'HARRIS_TEETER', -250, 'HARRIS'],
-    ['03/15/21', 'FARM_FRESH', -150, 'FRESH']]
+    'food': [
+        ['01/22/21', 'FOOD LION',  -200, 'FOOD LION'],
+        ['02/21/21', 'HARRIS_TEETER', -250, 'HARRIS'],
+        ['03/15/21', 'FARM_FRESH', -150, 'FRESH']]
 }

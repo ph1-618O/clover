cc152c5c733250ec9575782201a7e2853a24bd29
Jen S <j3n.s3idl@gmail.com>
2021-07-07 12:45:15 -0400

    working on location.py

--- location.py
+++ location.py
@@ -7,6 +7,8 @@
 import requests
 import json
 from config import gKey
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def find_location(target_query=None):
@@ -20,6 +22,10 @@ def find_location(target_query=None):
         if response.status_code == 200:
             #print('Success!')
             l_type = location["results"][0]["geometry"]['location_type']
+            # Saving this to add a map location for future applications
+            lat = location["results"][0]["geometry"]["location"]["lat"]
+            lng = location["results"][0]["geometry"]["location"]["lng"]
+            print(f'{target_query}: {lat}, {lng}')
             if l_type.lower() == 'approximate':
                 #print('returning true')
                 return True
@@ -32,14 +38,42 @@ def find_location(target_query=None):
         else:
             return 'ERROR'
         
-    # Saving this to add a map location for future applications
-    # lat = location["results"][0]["geometry"]["location"]["lat"]
-    # lng = location["results"][0]["geometry"]["location"]["lng"]
-    # print(f'{target_query}: {lat}, {lng}')
+
+def find_lat_lng(target_query=None):
+    if target_query:
+        #target_query = "VIRGINIA BEACVA"
+        params = {"address": target_query, "key": gKey}
+        base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+        #print(f'Testing {target_query}')
+        response = requests.get(base_url, params=params)
+        location = response.json()
+        if response.status_code == 200:
+            #print('Success!')
+            pp.pprint(location)
+            l_type = location["results"][0]["geometry"]['location_type']
+            # Saving this to add a map location for future applications
+            lat = location["results"][0]["geometry"]["location"]["lat"]
+            lng = location["results"][0]["geometry"]["location"]["lng"]
+            print(f'{target_query}: {lat}, {lng}')
+            if l_type.lower() == 'approximate':
+                #print('returning true')
+                return lat, lng
+            else:
+                #print('returning false')
+                return False
+        elif response.status_code == 404:
+            #print('Not Found.')
+            return False
+        else:
+            return 'ERROR'
 
 
 def main():
-    find_location()
+    #find_location()
+    found = find_lat_lng('Norfolk')
+    lat = found[0]
+    lng = found[1]
+    find_closest_cities(lat, lng)
 
 
 if __name__ == "__main__":

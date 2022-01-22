8d74597b4befffe031742b4ecc341fbd693552e5
Jen S <j3n.s3idl@gmail.com>
2022-01-21 22:29:30 -0500

    working on treemaps

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python3
 # coding: utf-8
-fgh
+
+""" ## Budget Rule
+- 50 % Needs
+- 30 % Wants
+- 20 % Savings
+Avg recommendations
+
+### Needs - 50 %
+- Home 20%
+- Utilities 5%
+- Food 10%
+- Transportation 5%
+- Medical 5%
+- Debt 5%
+
+### Wants - 30 %
+- Lifestyle 30%
+
+### Savings - 20%
+- Savings 20%
+ """
+ 
+import numpy as np
+import plotly.graph_objects as go
+import plotly.io as pio
+import pandas as pd
+import os
+
